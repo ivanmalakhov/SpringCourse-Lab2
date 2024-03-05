fc417faaa8f7ac365263697cc0d68f0ea3397ff2
Малахов Иван <malahovi@dev.vtb.ru>
2024-03-05 15:53:26 +0300

    Инициализация проекта

---
+++ src/main/java/ru/learn/springtask/SpringTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.learn.springtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskApplication.class, args);
+	}
+
+}
